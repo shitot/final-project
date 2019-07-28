477e1ab0ea0f78e4913068d0ff06c6edfae41aa7
Guy Golpur <37631562+Guygolpur@users.noreply.github.com>
2019-07-28 14:00:05 +0300

    Add files via upload

---
+++ Panel.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Control.h"
+class Panel :public Control
+{
+
+private:
+	vector<Control*> controls;
+	short width;
+	short height;
+
+public:
+	Panel(Border* border, short _left, short _top);
+
+	void draw(Graphics& g, int x, int y, size_t z);
+	void mousePressed(int x, int y, bool isLeft);
+	void keyDown(int keyCode, char charecter) {};
+	bool canGetFocus() { return TRUE; };
+	void addControlsToVector(Control* _control);
+	
+	Control* getFirstControl();
+
+	void getAllControls(vector<Control*> *controls);
+
+	void setWidth(short _width);
+	void setHeight(short _hige);
+	~Panel();
+};
+
